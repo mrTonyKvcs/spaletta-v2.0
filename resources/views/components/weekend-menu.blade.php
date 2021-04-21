c7d85b677abb7c3ac9bb713b407ba38ac254ff4b
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-04-21 16:43:36 +0200

    dynamic menus and wip gallery

--- resources/views/components/weekend-menu.blade.php
+++ resources/views/components/weekend-menu.blade.php
@@ -1,18 +1,12 @@
 <div class="lg:grid lg:grid-cols-2 lg:gap-24 text-gray48">
+    @foreach (config('spaletta.weekly-menu.weekendmenu') as $menu)
     <div class="flex flex-col items-center md:w-160 mb-16">
-        <h3 class="text-4xl uppercase pb-4 font-semibold text-gray2e">{{ __('Szombati Menü') }}</h3>
+        <h3 class="text-4xl uppercase pb-4 font-semibold text-gray2e">{{ __($menu['name']) }}</h3>
         <div class="w-full text-center divide-y divide-gray-300 font-semibold">
-            <p class="py-4 text-2xl">Zöldségleves (G, L)</p>
-            <p class="py-4 text-2xl w-full">Hortobágyi húsos palacsinta</p>
-            <p class="py-4 text-2xl">Kókuszkocka</p>
+            @foreach ($menu['items'] as $item)
+                <p class="py-4 text-2xl w-full">{{ $item }}</p>
+            @endforeach
         </div>
     </div>
-    <div class="flex flex-col items-center md:w-160 mb-16">
-        <h3 class="text-4xl uppercase pb-4 font-semibold text-gray2e">{{ __('Vasárnapi Menü') }}</h3>
-        <div class="w-full text-center divide-y divide-gray-300 font-semibold">
-            <p class="py-4 text-2xl">Tyúkhúsleves</p>
-            <p class="py-4 text-2xl w-full">Sertés bécsi – burgonyasaláta</p>
-            <p class="py-4 text-2xl">Somlói galuska</p>
-        </div>
-    </div>
+    @endforeach
 </div>
--- resources/views/components/weekly-menu.blade.php
+++ resources/views/components/weekly-menu.blade.php
@@ -1,19 +1,12 @@
 <div class="lg:grid lg:grid-cols-2 lg:gap-24 text-gray48">
+    @foreach (config('spaletta.weekly-menu.menu') as $menu)
     <div class="flex flex-col items-center md:w-160 mb-16">
-        <h3 class="text-4xl uppercase pb-4 font-semibold text-gray2e">{{ __('Levesek') }}</h3>
+        <h3 class="text-4xl uppercase pb-4 font-semibold text-gray2e">{{ __($menu['name']) }}</h3>
         <div class="w-full text-center divide-y divide-gray-300 font-semibold">
-            <p class="py-4 text-2xl">Citromos ragúleves (G)</p>
-            <p class="py-4 text-2xl w-full">Erőleves fridatto (G)</p>
-            <p class="py-4 text-2xl">Paradicsomleves (L)</p>
+            @foreach ($menu['items'] as $item)
+                <p class="py-4 text-2xl w-full">{{ $item }}</p>
+            @endforeach
         </div>
     </div>
-    <div class="flex flex-col items-center md:w-160 mb-16">
-        <h3 class="text-4xl uppercase pb-4 font-semibold text-gray2e">{{ __('Főételek') }}</h3>
-        <div class="w-full text-center divide-y divide-gray-300 font-semibold">
-            <p class="py-4 text-2xl">Rakott karfiol (G, L)</p>
-            <p class="py-4 text-2xl w-full">Citrusos harcsa roston – Hasábburgonya, (G, L)</p>
-            <p class="py-4 text-2xl">Máglyarakás</p>
-            <p class="py-4 text-2xl">Rántott sajt – rizs, tartár</p>
-        </div>
-    </div>
+    @endforeach
 </div>
