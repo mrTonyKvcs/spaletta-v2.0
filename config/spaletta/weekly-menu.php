01f69b5f4c453e6893ed05aa6a23f93e063fe866
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-05-24 11:00:45 +0200

    new weekly menu 05.25-05.29.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2021.05.18 - 2021.05.21.',
-    'weekend-date' => '2021.05.22.',
+    'menu-date' => '2021.05.25 - 2021.05.28.',
+    'weekend-date' => '2021.05.29.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Újházi – tyúkleves (L)',
-                'Spárgakrémleves, snidlinges ricottás pirítós (G, L)',
-                'Palócleves '
+                'Tarhonyaleves (L)',
+                'Hideg bazsalikomos paradicsomleves feta sajttal (G)',
+                'Tárkonyos pulyka raguleves (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Sous – vide csirkemell – rebarbarás almamártás, rizs (G, L)',
-                'Brassói aprópecsenye, ecetes vegyes saláta (G, L)',
-                'Tejszínes spenótos házi tészta-buggyantott fürjtojás (G, L)',
-                'Csörögefánk baracklekvár'
+                'Bakonyi sertésszelet – galuska, kovászos uborka (L)',
+                'Rántott csirkecomb – francia rakott burgonya, lilahagymás majonéz',
+                'Sóskafőzelék – posírozott tojás (L)',
+                'Rizskoch- erdei gyümölcsragu, fagyi (G)'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Hideg mentás meggyleves vanília fagyi (G)',
-                'Borjúnyak Budapest módra pirított polenta (G, L)',
-                'Epres túrós Pavlova (G)'
+                'Házi májgombócleves (L)',
+                'Csülök Pékné módra (G, L)',
+                'Áfonyás álom'
             ]
         ],
     ]
