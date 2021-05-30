2b5bec8d0d124c3877c97336e22c474333a1842a
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-05-30 21:50:34 +0200

    new wekkly menu 06.01-06-05

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2021.05.25 - 2021.05.28.',
-    'weekend-date' => '2021.05.29.',
+    'menu-date' => '2021.06.01 - 2021.06.04.',
+    'weekend-date' => '2021.06.05.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Tarhonyaleves (L)',
-                'Hideg bazsalikomos paradicsomleves feta sajttal (G)',
-                'Tárkonyos pulyka raguleves (G, L)'
+                'Francia tojásleves (L)',
+                'Gulyásleves (G, L)',
+                'Tavaszi salátaleves húsgombóccal'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bakonyi sertésszelet – galuska, kovászos uborka (L)',
-                'Rántott csirkecomb – francia rakott burgonya, lilahagymás majonéz',
-                'Sóskafőzelék – posírozott tojás (L)',
-                'Rizskoch- erdei gyümölcsragu, fagyi (G)'
+                'Spárgával töltött sertéstarja-burgonyakrém, almakompót',
+                'Bőrös fogas roston – pirított hajdina, mentás zöldborsó püré (G, L)',
+                'Rakott cukkini (G)',
+                'Eredeti cézársaláta'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Házi májgombócleves (L)',
-                'Csülök Pékné módra (G, L)',
-                'Áfonyás álom'
+                'Gombaleves kacsamájjal (G, L)',
+                'Rozé szűzpecsenye – langyos lencsesaláta, zöldbors mártás (G, L)',
+                'Lajcsi szelet'
             ]
         ],
     ]
