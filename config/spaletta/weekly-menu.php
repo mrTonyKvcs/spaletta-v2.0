f30e6d936f8d88501c24ba0a9a3b668786fd4d98
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-06-27 14:37:47 +0200

    new wekkly menu 06.29-07.02.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2021.06.22 - 2021.06.25.',
-    'weekend-date' => '2021.06.26.',
+    'menu-date' => '2021.06.29 - 2021.07.02.',
+    'weekend-date' => '2021.07.03.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Minestrone leves (L)',
-                'Hideg citromleves (L, G)',
-                'Babgulyás (L)'
+                'Hideg egresleves (G, L)',
+                'Zöldbableves kapros túrós galuskával',
+                'Magyaros gombaleves sertéshússal (L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Sous-vide csirkemell – gorgonzola mártás, spárgás rizs (G)',
-                'Töltött cukkini (G)',
-                'Zöldborsós tokány – galuska, meggykompót (L)',
-                'Zöldségtócsni - Bazsalikomos, újhagymás mártogatós (G, L)'
+                'Mexikói csirkés bulgur (L)',
+                'Marhapörkölt – túrós csusza, kovászos uborka',
+                'Roston harcsacsíkok – csőben sültkarfiol (G)',
+                'Nyári fetás saláta (G)'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Brokkoli krémleves, pirított mandula (G, L)',
-                'Pácolt sertéstarja – steak burgonya, friss saláta (G, L)',
-                'Citromos túrós pite'
+                'Hideg ánizsos szederkrémleves (G)',
+                'Sült pulykacomb – citromos pityóka (G, L)',
+                'Lime-os mentás süti'
             ]
         ],
     ]
