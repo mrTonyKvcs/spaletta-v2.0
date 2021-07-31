8bf5ec82819d19f8cab66a83e7ca575e3b50d255
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-07-31 16:18:15 +0200

    new weekly 08.03-08.07

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2021.07.27 - 2021.07.30.',
-    'weekend-date' => '2021.07.31.',
+    'menu-date' => '2021.08.03 - 2021.08.06.',
+    'weekend-date' => '2021.08.07.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Francia hagymakrém leves, sajtos toast kenyér',
-                'Tárkonyos pulyka raguleves citrommal (G, L)',
-                'Ázsiai zöldségleves (L)'
+                'Krémes gombaleves (G, L)',
+                'Marhagulyás leves (L)',
+                'Eperleves, panna cotta'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Hagyományos csevapcsicsa – fetás nyári saláta, sültburgonya (G)',
-                'Rántott camembert – jázminrizs, áfonyamártás',
-                'Hentes tokány – galuska (G, L)',
-                'Mákos guba – erdei gyümölcsragu'
+                'Brassói aprópecsenye, káposztasaláta (G, L)',
+                'Hideg tökfőzelék – amerikai bundás virsli (L)',
+                'Fehér csoki ganache – val töltött fánk',
+                'Cukkini fasírt – feketerizs, kevertsaláta (G, L)'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Hideg almaleves (G, L)',
-                'Harcsapaprikás steak – sült túrós csusza',
-                'Tökmagos vaníliafagylalt tejszínhabbal'
+                'Frankfurti leves (L)',
+                'Ropogós kacsacomb – meggymártás, mogyorós rizs (G, L)',
+                'Csokis gyümölcsöskosár'
             ]
         ],
     ]
