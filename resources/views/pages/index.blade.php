4f4bdc3be42facce83173b26c69dfdc54d52955a
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-09-06 22:54:09 +0200

    oktobierfest 2021

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -31,10 +31,16 @@
             <source src="../videos/spaletta-video.mp4" type="video/mp4">
         </video>
     </div>
-    {{-- <div class="flex flex-col items-center justify-center mt-56 px-6 text-4xl text-center">
-        <h2 class="font-semibold pb-4">Kedves Vendégeink !</h2>
-        <p>A mai napon 14.30-ig várjuk vendégeinket szombati menü ajánlatunkkal. 14.30-tól zártkörű rendezvény miatt éttermünk zárva tart.</p>
-    </div>  --}}
+    <div class="md:grid md:grid-cols-2 mt-56 px-6">
+        <div class="flex justify-end items-center">
+            <img class="lg:w-2/3" src="/images/events/2021-oktobierfest.jpg" alt="">
+        </div>
+        <div class="flex flex-col justify-center items-center space-y-8 mt-10 md:mt-0">
+            <h2 class="text-4xl text-center uppercase text-gray2e font-semibold">Oktobierfest a Spaletta Étteremben</h2>
+            <p class="text-3xl text-center font-medium gold font-libre italic">2021. szeptember 14 - 19.</p>
+            <x-button type="gray-transparent"><a href="{{ route('events.index') }}">{{ __('Részletek') }}</a></x-button>
+        </div>
+    </div> 
     <x-section>
         <div class="flex flex-col items-center mt-24">
             <x-title title="{{ __('Heti Menü') }}" text="{{ config('spaletta.weekly-menu.menu-date') }}" price="2100 Ft"></x-title>
