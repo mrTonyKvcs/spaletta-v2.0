9f3d3429190b33a199e30dd178d6741a1ad7f790
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-09-27 10:24:48 +0200

    new wekkly menu 09.28-10.02

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2021.09.21 - 2021.09.24.',
-    'weekend-date' => '2021.09.25.',
+    'menu-date' => '2021.09.28 - 2021.10.01.',
+    'weekend-date' => '2021.10.02.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Vietnámi marhahúsleves (G, L)',
-                'Mézes szőlőleves, citromos panna cotta (G)',
-                'Zöldborsóleves csurgatott galuskával (L)'
+                'Májgombócleves (L)',
+                'Palócleves (L)',
+                'Eperleves (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bácskai rizses hús, csemegeuborka (G, L)',
-                'Corn flakes bundás csirkemell – pároltzöldség, lilahagymás majonéz (G, L)',
-                'Penne arrabbiata',
-                'Harcsa papillote, Rác módra (G, L)',
+                'Gombás vaddisznóragu – Rizs (G, L)',
+                'Menü burger – hasábburgonya',
+                'Sült pulykacomb – törtburgonya, párolt savanyúkáposzta (G, L)',
+                'Vegán töltött paprika – paradicsommártás (L)',
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Kukorica krémleves, bacon chips (G, L)',
-                'Konfitált sertéstarja – steak burgonya, friss saláta (G, L)',
-                'Kávékrémes piskóta rolád'
+                'Nyírségi gombócleves (L)',
+                'Fűszeres rántott csirkecomb – bulgur, amerikai káposztasaláta (L)',
+                'Feketeerdő pohárkrém'
             ]
         ],
     ]
