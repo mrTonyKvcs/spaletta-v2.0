94835ada6e2f127fe7346e3d6a99a764eb1fdb23
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-11-12 21:42:21 +0100

    goose days index pages create

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -31,16 +31,16 @@
             <source src="../videos/spaletta-video.mp4" type="video/mp4">
         </video>
     </div>
-    {{-- <div class="md:grid md:grid-cols-2 mt-56 px-6">
+    <div class="md:grid md:grid-cols-2 mt-56 px-6">
         <div class="flex justify-end items-center">
-            <img class="lg:w-2/3" src="/images/events/2021-oktobierfest.jpg" alt="">
+            <img class="lg:w-2/3" src="/images/events/libahet.jpg" alt="">
         </div>
         <div class="flex flex-col justify-center items-center space-y-8 mt-10 md:mt-0">
-            <h2 class="text-4xl text-center uppercase text-gray2e font-semibold">Oktobierfest a Spaletta Étteremben</h2>
-            <p class="text-3xl text-center font-medium gold font-libre italic">2021. szeptember 14 - 19.</p>
+            <h2 class="text-4xl text-center uppercase text-gray2e font-semibold">Novemberi libahét a Spalettában!</h2>
+            <p class="text-3xl text-center font-medium gold font-libre italic">2021. november 16 - 21.</p>
             <x-button type="gray-transparent"><a href="{{ route('events.index') }}">{{ __('Részletek') }}</a></x-button>
         </div>
-    </div>  --}}
+    </div> 
     <x-section>
         <div class="flex flex-col items-center mt-24">
             <x-title title="{{ __('Heti Menü') }}" text="{{ config('spaletta.weekly-menu.menu-date') }}" price="2100 Ft"></x-title>
