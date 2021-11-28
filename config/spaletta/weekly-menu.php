b14f6ec4ccf73ca44dcf273155c22f5511da7415
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-11-28 13:43:02 +0100

    new wekkly menu 11.30-12.04

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2021.11.23 - 2021.11.26.',
-    'weekend-date' => '2021.11.27.',
+    'menu-date' => '2021.11.30 - 2021.12.03.',
+    'weekend-date' => '2021.12.04.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Zöldborsós csibeleves (G, L)',
-                'Körtés zellerkrémleves, pirított magvak (G, L)',
-                'Borscs leves (L)'
+                'Májgombócleves (L)',
+                'Vaddisznó raguleves (G, L)',
+                'Sült almás répa krémleves, répa chips (G, L)   '
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Óvári sertéskaraj – petrezselymes burgonya, barackbefőtt (G)',
-                'Sólett (G, L)',
-                'Rántott borjúmáj – karottás burgonyapüré, káposztával töltött almapaprika',
-                'Négysajtos penne',
+                'Harcsapaprikás – juhtúrós sztrapacska',
+                'Lasagne',
+                'Töltött csirkecomb – karfiolos kuszkusz (L)',
+                'Corn flakes bundába sült cukkini – hasábburgonya, fokhagymástejföl (G, L)',
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Szilvakrémleves, pudinggal, tejszínhabbal (G, L)',
-                'Roston sült csirkecomb filé – steak burgonya, friss saláta (G, L)',
-                'Rákóczi túrós'
+                'Lencseleves kapros krumpli gombóccal (L)',
+                'Lassan párolt sertéstarja – burgonyafánk (L)',
+                'Méteres szelet'
             ]
         ],
     ]
