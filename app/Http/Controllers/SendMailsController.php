8171c4ba4f0a42eb4089c13c294869cfbea3481a
tony <attila.kovacs92@gmail.com>
2022-04-19 20:11:54 +0200

    add exception to reservation

--- app/Http/Controllers/SendMailsController.php
+++ app/Http/Controllers/SendMailsController.php
@@ -109,7 +109,7 @@ class SendMailsController extends Controller
             return false;
         } elseif ($time < '11:30' || $time > '22:40') {
             return false;
-		} elseif ($checkin == '2022-03-15') {
+		} elseif ($checkin == '2022-04-29' && $time < '17:59') {
 			return false;
 		}
 
