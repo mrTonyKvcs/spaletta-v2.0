c59a23f8ebf9e1450f8ad3d600cf4baa894d68f5
KovacsJ92 <jozsef.kovacs92@gmail.com>
2022-05-16 13:43:57 +0200

    new weekly menu 05.17-05.21

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2022.05.10. - 2022.05.13.',
-    'weekend-date' => '2022.05.14.',
+    'menu-date' => '2022.05.17. - 2022.05.20.',
+    'weekend-date' => '2022.05.21.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Palócleves (L)',
-                'Könnyű tavaszi zöldségleves (G, L)',
-                'Gyümölcs krémleves, meggyfagyi (G)'
+                'Frankfurti leves (L)',
+                'Csibehúsos grízgaluskaleves (L)',
+                'Eper krémleves, mangó fagyi (G)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bakonyi sertésborda – galuska, uborkasaláta (L)',
-                'Zöldborsófőzelék – csibemáj felfújt (G, L)',
-                'Spárgás rizottó (G)',
-                'Sajtos brokkolis pulykamell – bulgur, csicseriborsó saláta'
+                'Zúzapörkölt – tarhonya, kovászos uborka (L)',
+                'Hideg tökfőzelék – arancini (L)',
+                'Rakott kelkáposzta (G, L)',
+                'Pulykás – spenótos tésztasaláta'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Legényfogó leves (G, L)',
-                'Vasalt csirkecomb file – hasáb, friss saláta (G, L)',
-                'Rizskoch, eper öntet'
+                'Tejfölös krumpligombóc leves füstölt hússal (L)',
+                'Ropogós kacsacomb, vörösboros meggymártás, burgonyapüré (G, L)',
+                'Rákóczi túrós pohárkrém'
             ]
         ],
     ]
