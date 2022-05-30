4bfc9967e68d687dc7d2737331f8424473612dd7
KovacsJ92 <jozsef.kovacs92@gmail.com>
2022-05-30 17:53:14 +0200

    new weekly menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2022.05.24. - 2022.05.27.',
-    'weekend-date' => '2022.05.28.',
+    'menu-date' => '2022.05.31. - 2022.06.03.',
+    'weekend-date' => '2022.06.04.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Legényfogó leves (G, L)',
-                'Csontleves csigatésztával (L)',
-                'Őszibarack krémleves (G, L)'
+                'Minestrone',
+                'Erőleves sertéshússal töltött ravioli (L)',
+                'Currys lencse krémleves pulykanyárs (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Brassói aprópecsenye, vegyes vágott savanyúság (G, L)',
-                'Rántott camembert – Basmati rizs, áfonyalekvár',
-                'Sültoldalas – parázsburgonya, friss saláta (G, L)',
-                'Roston harcsa – burgonyakrokett, hollandi mártás'
+                'Tarhonyás hús, tavaszi saláta (L)',
+                'Burgonyafőzelék – borjú fasírt (L)',
+                'Rántott zöldséggolyó – rusztikus bulgur, fokhagymás, kapros, tejföl (L)',
+                'Rizsfelfújt gyümölcsvelő'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Gulyásleves',
-                'Tojásos galuska – ecetes fejes saláta (L)',
-                'Rigójancsi'
+                'Lebbencsleves (L)',
+                'Tejszínes gombás pulyka – köles (G, L)',
+                'Házi krémes'
             ]
         ],
     ]
