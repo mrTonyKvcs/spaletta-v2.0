ba91787bf2f308d0a4d2f8529db1f727167233db
KovacsJ92 <jozsef.kovacs92@gmail.com>
2022-07-04 17:47:00 +0200

    new weekly menu 07.05 - 07.09

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2022.06.28. - 2022.07.01.',
-    'weekend-date' => '2022.07.02.',
+    'menu-date' => '2022.07.05. - 2022.07.08.',
+    'weekend-date' => '2022.07.09.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Raguleves (G,L)',
-                'Hideg Almaleves (G,L)',
-                'Bakonyi betyárleves (L)'
+                'Alföldi gulyás (L)',
+                'Vegyes gyümölcsleves (G, L)',
+                'Hideg tökleves (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Grillsajt-joghurtos saláta (G)',
-                'Szezámmagos harcsa-petrezselymes burgonya, tartár mártás (L)',
-                'Borjúpaprikás-galuska, uborka saláta (L)',
-                'Moussaka (görög rakott padlizsán) (G,L)'
+                'Francia lecsó (G, L)',
+                'Bolognai spagetti',
+                'Csirkés gyros (L)',
+                'Aranygaluska vanília sodó'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Húsleves gazdagon',
-                'Baconos pulykamell fetával töltve-burgonya püré, bazsalikomos paradicsomsaláta',
-                'Házi fagylalt'
+                'Minestrone',
+                'Mustáros flekken – steak burgonya, saláta',
+                'Citromos panna cotta'
             ]
         ],
     ]
