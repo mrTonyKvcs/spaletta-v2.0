55c26fd6848d5a2ee25a7132cc8b8be6be9e777a
KovacsJ92 <jozsef.kovacs92@gmail.com>
2022-07-17 15:12:23 +0200

    new weekly menu 07.19-07.23

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,24 +2,24 @@
 
 return [
     'title'     => 'heti menü',
-    'menu-date' => '2022.07.12. - 2022.07.15.',
-    'weekend-date' => '2022.07.16.',
+    'menu-date' => '2022.07.19. - 2022.07.22.',
+    'weekend-date' => '2022.07.23.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Hideg őszibarack krémleves, vanília fagylalt (G)',
-                'Kolbászos bableves',
-                'Hideg, fűszeres paradicsomleves (G, L)'
+                'Sárgadinnye krémleves (G)',
+                'Tojásleves (L)',
+                'Citromos csirke raguleves (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Hideg meggyszósz, magvas csirkefalatok',
-                'BBQ sertésoldalas, steakburgonya, nyári saláta (G, L)',
-                'Csicseriborsó fasírt, bulgur saláta (L)',
-                'Fettuccine primavera'
+                'Mustáros tarja – grillezett zöldség (G, L)',
+                'Rántott karfiol – rizibizi, tartár (L)',
+                'Baconös – cukkinis tészta',
+                'Hideg zöldbabfőzelék – sajtos húsgolyó rántva (L)'
             ]
         ],
     ],
@@ -27,9 +27,9 @@ return [
         'saturday-menu' => [
             'name'  => 'Szombati menü',
             'items' => [
-                'Erőleves húsos raviolival (L)',
-                'Csevapcsicsa, hasábburgonya, kevert saláta (G, L)',
-                'Tejes pite, csokoládé fagylalt'
+                'Frankfurti leves (G, L)',
+                'Rablóhús – kevertsaláta, joghurtöntettel (G, L)',
+                'Barackos pite'
             ]
         ],
     ]
