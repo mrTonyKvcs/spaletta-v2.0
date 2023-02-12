846a04be44b9444734cbc07561ea73f5217cd091
KovacsJ92 <jozsef.kovacs92@gmail.com>
2023-02-12 15:39:14 +0100

    crete index font

--- resources/views/components/weekly-menu.blade.php
+++ resources/views/components/weekly-menu.blade.php
@@ -1,12 +1,12 @@
 <div class="lg:grid lg:grid-cols-2 lg:gap-24 text-gray48 w-full my-12 space-y-10 md:space-y-0">
     @foreach (config('spaletta.weekly-menu.menu') as $menu)
     <div class="flex flex-col items-center md:w-full">
-        <h3 class="text-4xl md:mb-10 uppercase font-sembold text-gray2e">{{ __($menu['name']) }}</h3>
+        <h3 class="text-4xl uppercase font-sembold text-gray2e">{{ __($menu['name']) }}</h3>
         <div class="w-full divide-y divide-gray-300">
             @foreach ($menu['items'] as $item)
                 <div class="w-full py-4 flex flex-col md:flex-row items-center justify-between text-2xl">
                     <div class="">
-                        <p class="italic font-medium">{{ $item['name'] }}</p>
+                        <p class="italic font-medium text-center md:text-left">{{ $item['name'] }}</p>
                     </div>
                     <div class="">
                         <p class="font-semibold">{{ $item['price'] }} Ft</p>
