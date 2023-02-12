e5f392a5201efa4b0f0b950353071563c91980c5
KovacsJ92 <jozsef.kovacs92@gmail.com>
2023-02-12 15:55:54 +0100

    lang setup

--- resources/views/pages/bar.blade.php
+++ resources/views/pages/bar.blade.php
@@ -34,5 +34,12 @@
                     @endforelse
             </div>
         </div>
+        <div class="w-full flex flex-col items-center space-y-8">
+            <h1 class="text-2xl font-semibold">Chef: Kovács Attila</h1>
+            <div class="text-center">
+                {{ __('Allergének: 1. Laktóz, 2. Glutén, 3. Tojás,4. Rákok, Puhatestűek, 5. Hal, 6. Szója, 7. Olajos magvak,
+                8. Zeller, 9. Szezámmag, 10. Mustár') }}
+            </div>
+        </div>
     </x-section>
 </x-layout>
