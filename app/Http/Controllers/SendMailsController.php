9304aa9dc7ebd4e6c5d509ef65b520f20d6364d4
tony <attila.kovacs92@gmail.com>
2023-02-14 15:58:34 +0100

    add exception

--- app/Http/Controllers/SendMailsController.php
+++ app/Http/Controllers/SendMailsController.php
@@ -109,7 +109,7 @@ class SendMailsController extends Controller
             return false;
         } elseif ($time < '11:30' || $time > '22:40') {
             return false;
-        } elseif ($checkin <= '2023-01-09') {
+        } elseif ($checkin <= '2023-02-14') {
             return false;
         }
 
