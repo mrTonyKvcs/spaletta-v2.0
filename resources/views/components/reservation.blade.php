00fdc4d74e5788db0ae3e9aeadaec70a2c2e20ce
tony <attila.kovacs92@gmail.com>
2023-03-07 21:48:04 +0100

    delete carnival

--- resources/views/components/reservation.blade.php
+++ resources/views/components/reservation.blade.php
@@ -47,13 +47,7 @@
             <option value="Igen">{{ __('Igen') }}</option>
         </select> 
     </div>
-    <div x-show="checkin === '2023-02-25'" class="flex flex-col justify-between">
-        <p class="text-2xl font-medium text-center text-gold">{{ __('Asztalfoglalás a "Szülinapi farsang" eseményünkre') }}</p>
-        <select name="carnival" id="" class=" bg-white w-full gray outline-none text-center font-medium py-2.5 px-5 text-2xl border-b border-gray-300" required>
-            <option value="Nem">{{ __('Nem') }}</option>
-            <option value="Igen">{{ __('Igen') }}</option>
-        </select> 
-    </div>
+    
     <div class="flex flex-col justify-between">
         <p class="text-2xl font-medium text-center gray">{{ __('Megjegyzés') }}</p>
         <textarea name="comment" class="w-full gray outline-none text-center font-medium py-2.5 px-5 text-xl border-b border-gray-300"></textarea>
--- resources/views/email/template.blade.php
+++ resources/views/email/template.blade.php
@@ -29,11 +29,11 @@
     <p>
         <strong>Asztalfoglalás meccsnézésre:</strong> {{ $mail['matchmaker'] }}
     </p>
-    @if(isset($mail['carnival']))
+    {{-- @if(isset($mail['carnival']))
         <p>
             <strong>Asztalfoglalás a "Szülinapi farsang" eseményünkre:</strong> {{  $mail['carnival'] }}
         </p>
-    @endif
+    @endif --}}
     <p>
         <strong>Megjegyzés:</strong> {{ $mail['comment'] }}
     </p>
