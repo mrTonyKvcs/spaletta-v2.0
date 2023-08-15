f1345ca4eeac46db0102a8e99bdf831e00118f43
tony <attila.kovacs92@gmail.com>
2023-08-15 11:31:26 +0200

    reservayion

--- app/Http/Controllers/SendMailsController.php
+++ app/Http/Controllers/SendMailsController.php
@@ -111,7 +111,7 @@ class SendMailsController extends Controller
             return false;
         } elseif ($time < '11:30' || $time > '22:40') {
             return false;
-        } elseif ($checkin <= '2023-06-10') {
+        } elseif ($checkin >= '2023-08-20' && $checkin <= '2023-08-29') {
             return false;
         }
 
