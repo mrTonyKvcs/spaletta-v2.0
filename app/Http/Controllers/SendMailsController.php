914c9017bc9997a97ac409e39502f3a5b2003e37
kovacs.attila <kovacs.attila@arukereso.com>
2024-02-11 12:01:49 +0100

    turn of reservation

--- app/Http/Controllers/SendMailsController.php
+++ app/Http/Controllers/SendMailsController.php
@@ -111,7 +111,7 @@ class SendMailsController extends Controller
             return false;
         } elseif ($time < '11:30' || $time > '22:40') {
             return false;
-        } elseif ($checkin <= '2024-01-23') {
+        } elseif ($checkin === '2024-02-14') {
             return false;
         }
 
