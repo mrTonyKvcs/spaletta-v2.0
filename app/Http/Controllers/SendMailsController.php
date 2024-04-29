8ecf71257e80cf6f440a758bfa67771d8b9007cc
tony <attila.kovacs92@gmail.com>
2024-04-29 14:44:18 +0200

    finish

--- app/Http/Controllers/SendMailsController.php
+++ app/Http/Controllers/SendMailsController.php
@@ -111,7 +111,7 @@ class SendMailsController extends Controller
             return false;
         } elseif ($time < '11:30' || $time > '22:40') {
             return false;
-        } elseif ($checkin === '2024-04-12') {
+        } elseif ($checkin === '2024-05-03' && $time < '16:00') {
             return false;
         }
 
