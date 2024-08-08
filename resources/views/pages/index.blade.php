5c72095f908b90c5b1e91eae799e5f621ada6eb0
KovacsJ92 <jozsef.kovacs92@gmail.com>
2024-08-08 14:46:45 +0200

    new events

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -55,10 +55,10 @@
         <h2 class="text-4xl italic font-medium gold font-libre">Kedves Vendégeink !</h2>
         <p class="text-3xl italic font-medium text-center text-gray2e font-libre">A nyári szünet után , augusztus 30-án (kedden)  a megszokott nyitvatartással várjuk Vendégeinket</p>
     </div>  --}}
-    {{-- <div class="flex flex-col items-center justify-center w-full px-6 mt-56 space-y-8">
+    <div class="flex flex-col items-center justify-center w-full px-6 mt-56 space-y-8">
         <h2 class="text-4xl italic font-medium gold font-libre">Kedves Vendégeink !</h2>
-        <p class="text-3xl italic font-medium text-center text-gray2e font-libre">Éttermünk július 20-án szombaton, zártkörű rendezvény miatt zárva tart. Keddtől a megszokottak szerint várjuk Vendégeinket!</p>
-    </div> --}}
+        <p class="text-3xl italic font-medium text-center text-gray2e font-libre">Éttermünk augusztus 17 - 29 között, nyári szünet miatt zárva tart. Augusztus 30-án 16 órától a megszokottak szerint várjuk Vendégeinket!</p>
+    </div>
     {{-- <x-section background="christmas-section">
         <div class="flex justify-center">
             <div class="flex flex-col items-center justify-center mt-10 space-y-8 md:mt-0">
