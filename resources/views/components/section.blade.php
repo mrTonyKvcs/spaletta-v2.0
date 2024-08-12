d6d0804672c897ef5f75e23e2630411619774e7b
Attila Kovács <attila.kovacs@elementary-interactive.com>
2024-08-12 17:09:14 +0200

    f

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,8 +1,6 @@
-@props([
-    'background'
-])
+@props(['background'])
 
-<div class="{{ !empty($background) ? $background : '' }} px-6 py-32">
+<div class="{{ !empty($background) ? $background : '' }} px-6 py-32 w-full">
     <div class="lg:container lg:mx-auto flex flex-col items-center">
         {{ $slot }}
     </div>
