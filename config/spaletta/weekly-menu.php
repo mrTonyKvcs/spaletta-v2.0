cdd47afcf1712e5cff09ba4729583aec56a8fb9c
Tony Kovacs <attila.kovacs@elementary-interactive.com>
2025-06-02 10:09:02 +0200

    weekly

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,8 +2,8 @@
 
 return [
     'title'     => 'Déli Ajánló',
-    'menu-date' => '2025.05.28. - 2025.05.30.',
-    'saturday-date' => '2025.05.31.',
+    'menu-date' => '2025.06.04. - 2025.06.06.',
+    'saturday-date' => '2025.06.07.',
     'menu' => [
         // 'first-courses' => [
         //     'name' => 'Előétel',
@@ -18,12 +18,12 @@ return [
             'name' => 'Leves',
             'items' => [
                 [
-                    'name'  => 'Eperleves, mascarpone, bazsalikom(7)',
-                    'price' => 1450
+                    'name'  => 'Ribizli-málna, túrógombóc(1,3,7)',
+                    'price' => 1390
                 ],
                 [
-                    'name'  => 'Májgombóc leves(1,3,9)',
-                    'price' => 1490
+                    'name'  => 'Babkrémleves, füstölt kolbász, csipetke, tejföl(1,3,7,9)',
+                    'price' => 1470
                 ]
             ]
         ],
@@ -31,16 +31,16 @@ return [
             'name' => 'Főétel',
             'items' => [
                 [
-                    'name' => 'Zöldborsó főzelék, fasírt(1,3,7)',
-                    'price' => 2570
+                    'name' => 'Tarhonyás hús, csemege uborka(1,3,10)',
+                    'price' => 2640
                 ],
                 [
-                    'name' => 'Tagliatelle, lazac, spárga(1,3,4,7)',
-                    'price' => 2560
+                    'name' => 'Csirkeburger, hasábburgonya, fokhagymás majonéz(1,3,7)',
+                    'price' => 2780
                 ],
                 [
-                    'name' => 'Rántott tök, jázminrizs, tartár, dukkah(1,3,5,7,11)',
-                    'price' => 2380
+                    'name' => 'Rizottó, spárga, borsó(7)',
+                    'price' => 2490
                 ],
             ]
         ],
@@ -48,21 +48,21 @@ return [
             'name' => 'Desszert',
             'items' => [
                 [
-                    'name' => 'Epres vietnámi palacsinta, mézes joghurt(7)',
-                    'price' => 1230
+                    'name' => 'Almás-rebarbarás pite, tejszín(1,3,7)',
+                    'price' => 1280
                 ]
             ]
         ]
     ],
     'saturday-menu' => [
         'hide' => false,
-        'price' => 5290,
+        'price' => 5490,
         'items' => [
             'soup' => [
                 'name' => 'Leves',
                 'items' => [
                     [
-                        'name'  => 'Orjaleves, cérnametélt(1,4,9)',
+                        'name'  => 'Tojásleves, fűszeres, pirított kovászos kenyér kocka(1,3,9)',
                         'price' => 1500
                     ]
                 ]
@@ -71,7 +71,7 @@ return [
                 'name' => 'Főétel',
                 'items' => [
                     [
-                        'name' => 'Rántott csirkeszárny, petrezselymes burgonya, csemegeuborka(1,3,10)',
+                        'name' => 'Bbq sertésoldalas, steakburgonya, coleslaw(1,3,6,7,10)',
                         'price' => 2500
                     ],
                 ]
@@ -80,7 +80,7 @@ return [
                 'name' => 'Desszert',
                 'items' => [
                     [
-                        'name' => 'Tejespite, levendulás baracklekvár(1,3,7)',
+                        'name' => 'Tiramisu(1,3,7)',
                         'price' => 1200
                     ]
                 ]
