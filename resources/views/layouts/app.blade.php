90de79457f850f9c442d9f63d863a82777ebb72f
Tony Kovacs <attila.kovacs@elementary-interactive.com>
2025-08-18 15:45:58 +0200

    navbar text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,49 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
-        @livewireStyles
+    @livewireStyles
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DXE05BF6Y9"></script>
-        <script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DXE05BF6Y9"></script>
+    <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'G-DXE05BF6Y9');
-        </script>
+    </script>
 
-        
+
 </head>
-<body >
+
+<body>
     <header>
         <livewire:navbar />
         {{-- @include('partials.new-navbar') --}}
     </header>
     <main class="z-10 bg-white shadow-lg mb-160">
         {{-- <div class="timer" x-data="timer(new Date().setDate(new Date().getDate() + 1))" x-init="init();"> --}}
-            {{-- <h1 x-text="time().days"></h1> --}}
-            {{-- <h1 x-text="time().hours"></h1> --}}
-            {{-- <h1 x-text="time().minutes"></h1> --}}
-            {{-- <h1 x-text="time().seconds"></h1> --}}
-          {{-- </div> --}}
-       @yield('content')
+        {{-- <h1 x-text="time().days"></h1> --}}
+        {{-- <h1 x-text="time().hours"></h1> --}}
+        {{-- <h1 x-text="time().minutes"></h1> --}}
+        {{-- <h1 x-text="time().seconds"></h1> --}}
+        {{-- </div> --}}
+        @yield('content')
     </main>
     <footer class="light-gray-bg footer-parallax">
         @include('partials.footer')
@@ -48,4 +53,5 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
 
 </body>
+
 </html>
