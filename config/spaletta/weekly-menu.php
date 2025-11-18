ef8ffbdd6d12eedfcba23e24f4db5d96ef025076
Tony Kovacs <attila.kovacs@elementary-interactive.com>
2025-11-18 08:50:25 +0100

    weekly

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,8 +2,8 @@
 
 return [
     'title'     => 'Déli Ajánló',
-    'menu-date' => '2025.11.12. - 2025.11.14.',
-    'saturday-date' => '2025.11.15.',
+    'menu-date' => '2025.11.19. - 2025.11.21.',
+    'saturday-date' => '2025.11.22.',
     'hide' => false,
     'menu' => [
         // 'first-courses' => [
@@ -20,11 +20,11 @@ return [
             'items' => [
                 [
                     'name'  => 'Tojásleves(3,9)',
-                    'price' => 1320
+                    'price' => 1560
                 ],
                 [
-                    'name'  => 'Nyírségi gombócleves(1,3,7)',
-                    'price' => 1520
+                    'name'  => 'Grízgaluska leves(1,3)',
+                    'price' => 1440
                 ]
             ]
         ],
@@ -32,16 +32,16 @@ return [
             'name' => 'Főétel',
             'items' => [
                 [
-                    'name' => 'Erdélyi rakott káposzta(7)',
-                    'price' => 2580
+                    'name' => 'Mustáros flekken,steak burgonya,lyoni hagyma(1,10)',
+                    'price' => 2540
                 ],
                 [
-                    'name' => 'Rántott csirkemell, rizibizi, almakompót(1,3,7)',
-                    'price' => 2520
-                ],
-                [
-                    'name' => 'Harisszás karfiol, sült zöldségek, hummusz, sós citrom(11)',
+                    'name' => 'Rántott csirkemáj, waldorf saláta(1,3,7,8,9,10)',
                     'price' => 2460
+                ],
+                [
+                    'name' => 'Borsópörkölt,galuska(1,3)',
+                    'price' => 2380
                 ],
             ]
         ],
@@ -49,21 +49,21 @@ return [
             'name' => 'Desszert',
             'items' => [
                 [
-                    'name' => 'Meggyes pite, tejszín(1,3,7)',
-                    'price' => 1380
+                    'name' => 'Túrógombóc,tejföl(1,3,7)',
+                    'price' => 1360
                 ]
             ]
         ]
     ],
     'saturday-menu' => [
         'hide' => false,
-        'price' => 5480,
+        'price' => 5380,
         'items' => [
             'soup' => [
                 'name' => 'Leves',
                 'items' => [
                     [
-                        'name'  => 'Borsóleves, reszelt tészta(1,3,7)',
+                        'name'  => 'Tyúkhúsleves,cérnametélt(1,3,9)',
                         'price' => 1500
                     ]
                 ]
@@ -72,7 +72,7 @@ return [
                 'name' => 'Főétel',
                 'items' => [
                     [
-                        'name' => 'Lasagne(1,3,7,9)',
+                        'name' => 'Székelykáposzta,tejföl(7)',
                         'price' => 2500
                     ],
                 ]
@@ -81,7 +81,7 @@ return [
                 'name' => 'Desszert',
                 'items' => [
                     [
-                        'name' => 'Szilvás cobbler, tejszín(1,3,7)',
+                        'name' => 'Almás palacsinta,vanília sodó(1,3,7)',
                         'price' => 1200
                     ]
                 ]
